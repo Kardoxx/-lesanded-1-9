7a38203bd24b02a7da4e2f6f2422805b3d87d522
Kxrdo <115535255+Kardoxx@users.noreply.github.com>
2023-03-02 00:33:31 +0200

    Add files via upload

---
+++ kardo_kodutöö.py
@@ -0,0 +1,6 @@
+#Kardo Tamm
+a = 1  # annab a-le väärtuse 1
+b = 'a/n'  # annab b-le sõneväärtuse a/n
+while a < 3:  # kui a on väiksem kui 3
+    print(b)  # prindib b
+    a += 1  # ja inkremendib a-d 1-ga
