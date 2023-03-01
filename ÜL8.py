d00e6846bafeffc67f1dba18eeb8c8a6ee1f6f13
Kxrdo <115535255+Kardoxx@users.noreply.github.com>
2023-03-02 00:39:56 +0200

    Add files via upload

---
+++ ÜL8.py
@@ -0,0 +1,5 @@
+#Kardo Tamm
+a = 10  # a- le antakse väärtus 10
+b = 2  # b- le antakse väärtus 2
+print(a != b and b > 10)  # prindib a ei ole võrdne b-ga ja b on suurem kui 10
+print(b == a or a > 0)  # prindib b on võrdeline a- ga või a on suurem kui 0
