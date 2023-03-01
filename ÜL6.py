6cac10ab3c74031de24fe9577a5611bb07b4b789
Kxrdo <115535255+Kardoxx@users.noreply.github.com>
2023-03-02 00:38:38 +0200

    Add files via upload

---
+++ ÜL6.py
@@ -0,0 +1,6 @@
+#Kardo Tamm
+a = "A"  # sõna 1
+b = 1  # b - le antakse väärtuseks 1
+while b < 3:  # kui b on väiksem, kui kolm
+    print(a, end=" ")  # prindib a ning lisab lõppu tühiku
+    b += 1  # inkremendib b 1 võrra
