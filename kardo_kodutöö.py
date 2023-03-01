6c8cb872135be88b9dc07bc65b54d2d257dbaef7
Kxrdo <115535255+Kardoxx@users.noreply.github.com>
2023-03-02 00:34:38 +0200

    Delete kardo_kodutöö.py

--- kardo_kodutöö.py
+++
@@ -1,6 +0,0 @@
-#Kardo Tamm
-a = 1  # annab a-le väärtuse 1
-b = 'a/n'  # annab b-le sõneväärtuse a/n
-while a < 3:  # kui a on väiksem kui 3
-    print(b)  # prindib b
-    a += 1  # ja inkremendib a-d 1-ga
